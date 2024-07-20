230ce2837138a8f395a246ed112145de7c13ef96
tarilabs <matteo.mortari@gmail.com>
2024-07-20 12:38:41 +0200

    Initial commit
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>

---
+++ README.md
@@ -0,0 +1,58 @@
+<!-- migrating from https://github.com/tarilabs/oml -->
+
+Using OCI Artifact for ML model and metadata
+
+# Installation
+
+```
+pip install omlmd
+```
+
+# Push
+
+```py
+from omlmd.helpers import Helper
+
+omlmd = Helper()
+omlmd.push("localhost:8080/matteo/ml-artifact:latest", "model.joblib", name="Model Example", author="John Doe", license="Apache-2.0", accuracy=9.876543210)
+```
+
+# Pull
+
+```py
+omlmd.pull(target="localhost:8080/matteo/ml-artifact:latest", outdir="tmp/b", media_types=["application/x-artifact"])
+```
+
+# Custom Pull: only metadata layer
+
+Implementation intends to follow OCI-Artifact convention
+
+```py
+md = omlmd.get_config(target="localhost:8080/matteo/ml-artifact:latest")
+print(md)
+```
+
+# Crawl
+
+Client-side crawling of metadata.
+
+Note: server-side analogous, reference in blueprints.
+
+```py
+crawl_result = omlmd.crawl([
+    "localhost:8080/matteo/ml-artifact:v1", "localhost:8080/matteo/ml-artifact:v2", "localhost:8080/matteo/ml-artifact:v3"
+])
+```
+
+## Example query
+
+Demonstrate integration of crawling results with querying (in this case using jQ)
+
+> Of the crawled ML OCI artifacts, which one exhibit the max accuracy?
+
+```py
+import jq
+jq.compile( "max_by(.config.customProperties.accuracy).reference" ).input_text(crawl_result).first()
+```
+
+# To be continued...
---
+++ omlmd/helpers.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass, field, asdict, fields
+from typing import Optional, Dict, Any, List
+from omlmd.model_metadata import ModelMetadata
+from oras.provider import Registry
+from omlmd.provider import OMLMDRegistry
+import os
+import urllib.request
+
+def write_content_to_file(filename: str, content_fn):
+    try:
+        with open(filename, 'x') as f:
+            content = content_fn()
+            f.write(content)
+    except FileExistsError:
+        raise RuntimeError(f"File '{filename}' already exists. Aborting TODO: demonstrator.")
+
+
+def download_file(uri):
+    file_name = os.path.basename(uri)
+    urllib.request.urlretrieve(uri, file_name)
+    return file_name
+
+
+class Helper:
+    def __init__(self, registry: Optional[OMLMDRegistry] = None):
+        if registry is None:
+            self._registry = OMLMDRegistry(insecure=True)
+        else:
+            self._registry = registry
+
+    @property
+    def registry(self):
+        return self._registry
+
+    def push(
+        self,
+        target: str,
+        path: str,
+        name: Optional[str] = None,
+        description: Optional[str] = None,
+        author: Optional[str] = None,
+        model_format_name: Optional[str] = None,
+        model_format_version: Optional[str] = None,
+        **kwargs
+    ):
+        dataclass_fields = {f.name for f in fields(ModelMetadata)} # avoid anything specified in kwargs which would collide
+        custom_properties = {k: v for k, v in kwargs.items() if k not in dataclass_fields}
+        model_metadata = ModelMetadata(
+            name=name,
+            description=description,
+            author=author,
+            customProperties=custom_properties,
+            model_format_name=model_format_name,
+            model_format_version=model_format_version
+        )
+        write_content_to_file("model_metadata.omlmd.json", lambda: model_metadata.to_json())
+        write_content_to_file("model_metadata.omlmd.yaml", lambda: model_metadata.to_yaml())
+        files = [
+            f"{path}:application/x-artifact",
+            "model_metadata.omlmd.json:application/x-config",
+            "model_metadata.omlmd.yaml:application/x-config",
+        ]
+        try:
+            return self._registry.push(
+                target=target,
+                files=files,
+                manifest_annotations=model_metadata.to_annotations_dict(),
+                manifest_config="model_metadata.omlmd.json:application/x-config"
+            )
+        finally:
+            os.remove("model_metadata.omlmd.json")
+            os.remove("model_metadata.omlmd.yaml")
+
+
+    def pull(
+        self,
+        target: str,
+        outdir: str,
+        media_types: List[str]
+    ):
+        self._registry.download_layers(target, outdir, media_types)
+
+
+    def get_config(
+        self,
+        target: str
+    ) -> str:
+        return f'{{"reference":"{target}", "config": {self._registry.get_config(target)} }}' # this assumes OCI Manifest.Config later is JSON (per std spec)
+
+
+    def crawl(
+        self,
+        targets: List[str]
+    ) -> str:
+        configs = map(self.get_config, targets)
+        joined = "[" + ", ".join(configs) + "]"
+        return joined
