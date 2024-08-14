168bd5ccd3113275b60f7508a3423a5fd130a1b2
Isabella do Amaral <idoamara@redhat.com>
2024-08-14 08:27:35 -0300

    helpers: save json and yaml md on path
    Signed-off-by: Isabella do Amaral <idoamara@redhat.com>

--- omlmd/helpers.py
+++ omlmd/helpers.py
@@ -11,18 +11,7 @@ from omlmd.model_metadata import ModelMetadata
 from omlmd.provider import OMLMDRegistry
 
 
-def write_content_to_file(filename: str, content_fn):
-    try:
-        with open(filename, "x") as f:
-            content = content_fn()
-            f.write(content)
-    except FileExistsError:
-        raise RuntimeError(
-            f"File '{filename}' already exists. Aborting TODO: demonstrator."
-        )
-
-
-def download_file(uri):
+def download_file(uri: str):
     file_name = os.path.basename(uri)
     urllib.request.urlretrieve(uri, file_name)
     return file_name
@@ -68,16 +57,23 @@ class Helper:
             model_format_name=model_format_name,
             model_format_version=model_format_version,
         )
-        write_content_to_file(
-            "model_metadata.omlmd.json", lambda: model_metadata.to_json()
-        )
-        write_content_to_file(
-            "model_metadata.omlmd.yaml", lambda: model_metadata.to_yaml()
-        )
+        if isinstance(path, str):
+            path = Path(path)
+
+        json_meta = path.parent / "model_metadata.omlmd.json"
+        yaml_meta = path.parent / "model_metadata.omlmd.yaml"
+        if (p := json_meta).exists() or (p := yaml_meta).exists():
+            raise RuntimeError(
+                f"File '{p}' already exists. Aborting TODO: demonstrator."
+            )
+        json_meta.write_text(model_metadata.to_json())
+        yaml_meta.write_text(model_metadata.to_yaml())
+
+        manifest_cfg = f"{json_meta}:application/x-config"
         files = [
             f"{path}:application/x-mlmodel",
-            "model_metadata.omlmd.json:application/x-config",
-            "model_metadata.omlmd.yaml:application/x-config",
+            manifest_cfg,
+            f"{yaml_meta}:application/x-config",
         ]
         try:
             # print(target, files, model_metadata.to_annotations_dict())
@@ -85,13 +81,13 @@ class Helper:
                 target=target,
                 files=files,
                 manifest_annotations=model_metadata.to_annotations_dict(),
-                manifest_config="model_metadata.omlmd.json:application/x-config",
+                manifest_config=manifest_cfg,
             )
             self.notify_listeners(PushEvent(target, model_metadata))
             return result
         finally:
-            os.remove("model_metadata.omlmd.json")
-            os.remove("model_metadata.omlmd.yaml")
+            json_meta.unlink()
+            yaml_meta.unlink()
 
     def pull(
         self, target: str, outdir: Path | str, media_types: Sequence[str] | None = None
